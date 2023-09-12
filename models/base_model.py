2c553bceb504178591360adb66cd39778c46ee7c
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-09-12 18:30:10 +0300

    models/base_model.py - 02

--- models/base_model.py
+++ models/base_model.py
@@ -9,20 +9,42 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         generate a unique id usin uuid.uuid4()
         -> You can use uuid.uuid4() to generate unique id but don’t
         forget to convert to a string.
         -> The goal is to have unique id for each BaseModel
         """
-        self.id = str(uuid.uuid4())
 
-        # set the created_at attribute to the current datetime
-        self.created_at = datetime.now()
+        """We are updating the init method to re-create an instance
+        with this dictionary representation
+        """
 
-        # set the updated_at attribute to the same value as the created at
-        self.updated_at = self.created_at
+        if kwargs:
+            # iterate ove the key_value pairs in kwargs
+            for key, value in kwargs.items():
+                
+                # skip the __class__ attribute
+                if key == '__class__':
+                    continue
+                # convert the created and updated intp datetime objects
+                # this helps them be easier to work with and
+                # compatible with program
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                
+                # set the attribute on the instance
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+
+            # set the created_at attribute to the current datetime
+            self.created_at = datetime.now()
+
+            # set the updated_at attribute to the same value as the created at
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
