6dba8f6a94261f226bc2ea7e69b6a68296496c64
JenMora <jeniffermourah73@gmail.com>
2023-09-13 09:45:35 +0300

    This Python script is designed to perform unit tests for the Amenity class in the context of a larger application

---
+++ tests/test_amenity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module defines unit tests for amenity.py
+"""
+
+import unittest
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    This class tests cases for the Amenity class
+    """
+
+    def test_amenity_creation(self):
+        """
+        This method tests if an Amenity object can be created with a default
+        name
+        """
+        item = Amenity()
+        self.assertEqual(item.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
