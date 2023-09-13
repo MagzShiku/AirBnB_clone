49505ecd5f748660dab4c7beb2c41b8e679ac611
JenMora <jeniffermourah73@gmail.com>
2023-09-13 08:59:02 +0300

    This code is a Python script that defines a series of tests for a Python class called BaseModel. The purpose of these tests is to check whether the BaseModel class behaves as expected. These tests are written using the unittest library, which is a built-in Python library for writing and running tests.

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This is a module that defines the BaseModel class
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+import os
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test class for the BaseModel class
+    """
+
+    def setUp(self):
+        """
+        Set up a fresh BaseModel instance for each test.
+        """
+        self.base_model = BaseModel()
+
+    def tearDown(self):
+        """
+        Clean up resources after each test if necessary.
+        """
+        del self.base_model
+        # Optionally, you can remove the 'file.json' created during testing.
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+
+    def test_save(self):
+        """
+        Test the save method of BaseModel.
+        """
+        first_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        second_updated_at = self.base_model.updated_at
+
+        self.assertTrue(second_updated_at > first_updated_at)
+
+    def test_to_dict(self):
+        """
+        Test the to_dict method of BaseModel.
+        """
+        obj_dict = self.base_model.to_dict()
+
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+        self.assertIsInstance(obj_dict["created_at"], str)
+        self.assertIsInstance(obj_dict["updated_at"], str)
+
+        # You can also check if the keys are present or not if needed.
+        self.assertIn("id", obj_dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
