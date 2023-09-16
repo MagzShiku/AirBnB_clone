1ff41a167147f831ba21a4fa1fe3487075adced4
JenMora <jeniffermourah73@gmail.com>
2023-09-16 13:05:23 +0300

    This is a Amenity class that inherits from BaseModel with public class attributes item:string - empty string

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a module that defines the amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+   This is a  class that represents amenities
+   Arguments: a sting
+    """
+    item = ""
